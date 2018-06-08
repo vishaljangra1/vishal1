68501f774543b67d9039f3de3f4fae0ed5dd5766
Your Name <you@example.com>
2018-06-08 19:59:01 +0530

    first commit

---
+++ app/src/main/java/com/example/vishaljangra/myapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.vishaljangra.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
